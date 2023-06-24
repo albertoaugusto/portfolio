0b69f60377cf86b0eabd75b5d4a19905538d2f7c
Alberto Souza <albertosouza16.contato@gmail.com>
2023-06-24 07:48:41 -0300

    main init and styled components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,23 @@
-
+import './home.css'
+import Social from './Social'
+import Data from './Data'
+import Scroll from './Scroll'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home section">
+        <div className="home-container container grid">
+            <div className="home-content grid">
+                <Social />
+
+                <div className="home-img"></div>
+
+                <Data />    
+            </div>
+
+            <Scroll />
+        </div>
+    </section>
   )
 }
 
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,15 @@
+
+const Social = () => {
+  return (
+   <div className="home-social">
+        <a href="https://www.linkedin.com/in/alberto-souza-b4a1451b5/" rel="noreferrer"className="home-social-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/albertoaugusto" rel="noreferrer" className="home-social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+   </div>
+  )
+}
+
+export default Social
