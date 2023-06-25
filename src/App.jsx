a02824678d2f2b9934d4290a3775efea7c829a3c
Alberto Souza <albertosouza16.contato@gmail.com>
2023-06-25 04:46:39 -0300

    about component end and some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import About from './components/about/About'
 
 function App () {
   return (
@@ -8,8 +9,9 @@ function App () {
     <Header />
     <main className="main">
       <Home />
+      <About />
     </main>
-    
+
     </>
   )
 }
---
+++ src/components/about/Infos.jsx
@@ -0,0 +1,37 @@
+
+
+const Infos = () => {
+  return (
+   <div className="about-info grid">
+        <div className="about-box">
+            <i className="bx bx-award about-icon"></i>
+            <h3 className="about-title">
+                Experiência
+            </h3>
+            <span className="about-subtitle">
+                +1 Ano
+            </span>
+        </div>
+        <div className="about-box">
+            <i className="bx bx-briefcase about-icon"></i>
+            <h3 className="about-title">
+                Completos
+            </h3>
+            <span className="about-subtitle">
+                10+ Projetos                 
+            </span>
+        </div>
+        <div className="about-box">
+            <i className="bx bx-certification about-icon"></i>
+            <h3 className="about-title">
+                Certificados
+            </h3>
+            <span className="about-subtitle">
+                Fullstack Developer OneBitCode                 
+            </span>
+        </div>
+   </div>
+  )
+}
+
+export default Infos
