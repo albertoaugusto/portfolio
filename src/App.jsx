e1b51e888b63d4b955f309eb6cef846b497dbe2a
Alberto Souza <albertosouza16.contato@gmail.com>
2023-06-27 21:57:35 -0300

    contact form component end and emailjs api integrated

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from './components/home/Home'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
 import Services from './components/services/Services'
-import Contact from './components/contact/Contact'
 import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
 
 function App () {
   return (
